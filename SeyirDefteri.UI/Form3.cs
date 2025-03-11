33a226359986e5c6931af619abd60f1e0010da3e
BurakSenol00 <buraksenol00@outlook.com>
2025-03-11 16:47:35 +0300

    Moves lists to the other page

---
+++ SeyirDefteri.UI/Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SeyirDefteri.Core.Classlar;
+
+namespace SeyirDefteri.UI
+{
+   
+    public partial class Form3: Form
+    {
+       private List<Gonderim> Gonderimler;
+        public Form3() 
+        {
+            InitializeComponent();
+        }
+        public Form3(List<Gonderim>gonderimler) : this()
+        {
+            Gonderimler = gonderimler;
+        }
+    }
+}
